09ca75c130788e12ca8b1f23eca46cd6057b9873
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2023-01-31 09:28:29 +0100

    pendu pygame

---
+++ outils.py
@@ -0,0 +1,131 @@
+import random
+import pygame
+import string
+from tkinter import *
+
+
+class Bouton():
+    def __init__(self, message, x, y):
+        self.texte = message
+        self.rect = self.texte.get_rect()
+        self.rect.topleft = (x, y)
+        self.rect.w = self.texte.get_width()+20
+        self.rect.h = self.texte.get_height()+20
+
+    def affichage(self, surface):
+        action = False
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+
+            if (pygame.mouse.get_pressed()[0] == 1):
+                action = True
+
+        pygame.draw.rect(surface, 'red', self.rect, 2)
+        surface.blit(self.texte, ( self.rect.x+10, self.rect.y+10))
+
+        return action
+
+
+def partie(mot, trouve):
+
+    retour = []
+
+    for lettre in mot:
+        
+        if lettre == " ":
+            retour += ["  "]
+
+        elif lettre.lower() in trouve:
+            retour += [lettre]
+
+        else:
+            retour += ["_"]
+
+    return " ".join(retour)
+
+
+def mot_hasard(niveau):
+
+    match niveau:
+        case 1:
+            f = open("pendu/dictionnaires/motsFacile.txt", "r")
+        case 2:
+            f = open("pendu/dictionnaires/motsMedium.txt", "r")
+        case 3:
+            f = open("pendu/dictionnaires/motsDifficile.txt", "r")
+        case _:
+            f = open("pendu/dictionnaires/mots.txt", "r")
+
+    mots = f.read().splitlines()
+    f.close()
+
+    return random.choice(mots)
+
+
+def recup_scores(police):
+
+    topdix = []
+
+    f = open("pendu/scores.txt", "r")
+    scores = f.read().splitlines()
+
+    for x in range(len(scores)):
+        scores[x] = scores[x].split(",")
+        scores[x][1] = int(scores[x][1])
+
+    scores.sort(reverse= True, key = lambda score: score[1])
+
+    i = 0
+
+    while True:
+
+        if i == 10 or i >= (len(scores)):
+            break
+        topdix += [police.render(f"{scores[i][0]}  :  {scores[i][1]}", 1, 'black')]
+
+        i+=1
+        
+
+    return topdix
+
+
+def ajout_score(st_pendu, mot, nom):
+    score_final = (10-st_pendu)*len(mot)
+    f = open("pendu/scores.txt", "a")
+    if len(nom) > 0:
+        f.write(f"{nom}, {score_final}\n")
+    else:
+        f.write(f"Anon, {score_final}\n")
+    f.close()
+
+
+class Entree():
+
+    def __init__(self, x, y, police):
+        self.texte = ''
+        self.rect = pygame.Rect(x, y, 100, 40)
+        self.surface = police.render(self.texte, 1, 'black')
+        self.rect.h = self.surface.get_height()+20
+
+    def affichage(self, surface, police):
+
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+
+            for event in pygame.event.get():
+
+                if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_BACKSPACE:
+                        self.texte = self.texte[:-1]
+
+                    elif pygame.key.name(event.key) in string.ascii_lowercase:
+                        self.texte += pygame.key.name(event.key)
+
+                self.surface = police.render(self.texte, 1, 'black')
+                self.rect.w = max(100 ,self.surface.get_width()+20)
+        
+        pygame.draw.rect(surface, 'lightblue', self.rect, 2)
+        surface.blit(self.surface, (self.rect.x+10, self.rect.y+10))
---
+++ pendu.py
@@ -0,0 +1,197 @@
+from outils import *
+import pygame
+import string
+
+pygame.init()
+
+ecran = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Pendu")
+police_grand = pygame.font.SysFont('Verdana', 40)
+police_petit = pygame.font.SysFont('Verdana', 20)
+clock = pygame.time.Clock()
+
+
+solution = mot_hasard(0)
+scores = recup_scores(police_petit)
+nom = Entree(425, 500, police_petit)
+mot_manuel = Entree(475, 300, police_petit)
+statut_pendu = 0
+statut_partie = 0
+correct = []
+erreurs = []
+
+image = []
+for i in range(10):
+    image += [pygame.image.load(f"pendu/images/pendu{i}.png")]
+
+
+def ecran_jeu():
+
+    ecran.fill((255, 255, 255))
+    
+    jeu = police_grand.render(partie(solution, correct), 1, 'black')
+    reponse_fausse = police_petit.render(f"Erreurs: {' '.join(erreurs)}", 1, 'black')
+
+    ecran.blit(jeu, (325,300))
+    ecran.blit(reponse_fausse, (50,500))
+    ecran.blit(image[statut_pendu], (0, 100))
+
+
+def ecran_menu():
+    global statut_partie, solution, nom
+
+    ecran.fill((255, 255, 255))
+
+    Facile = police_petit.render("FACILE", 1, 'black')
+    medium = police_petit.render("MOYEN", 1, 'black')
+    difficile = police_petit.render("DIFFICILE", 1, 'black')
+    manuel = police_petit.render("MANUEL : ", 1, 'black')
+    scores = police_petit.render("Scores", 1, 'black')
+    lbl_nom = police_petit.render("Nom : ", 1, 'black')
+    
+    btn_Facile = Bouton(Facile, 200, 200)
+    btn_medium = Bouton(medium, 350, 200)
+    btn_difficile = Bouton(difficile, 500, 200)
+    btn_manuel = Bouton(manuel, 330, 300)
+    btn_scores = Bouton(scores, 150, 500)
+
+    nom.affichage(ecran, police_petit)
+    mot_manuel.affichage(ecran, police_petit)
+    ecran.blit(lbl_nom, (350, 510))
+
+    if btn_Facile.affichage(ecran):
+        solution = mot_hasard(1)
+        statut_partie = 1
+
+    if btn_medium.affichage(ecran):
+        solution = mot_hasard(2)
+        statut_partie = 1
+
+    if btn_difficile.affichage(ecran):
+        solution = mot_hasard(3)
+        statut_partie = 1
+
+    if btn_manuel.affichage(ecran):
+        if  2 < len(mot_manuel.texte) < 13:
+            solution = mot_manuel.texte
+            statut_partie = 1
+
+    if btn_scores.affichage(ecran):
+        statut_partie = 3
+
+
+def ecran_fin():
+
+    global statut_partie
+
+    if victoire():
+        message = "Felicitation!"
+    else:
+        message = "Dommage..."
+
+    reponse = police_petit.render(f"Le mot etait : {solution}", 1, 'black')
+    ecran.fill((255, 255, 255))
+    
+    menu = police_petit.render("Menu", 1, 'black')
+    scores = police_petit.render("Scores", 1, 'black')
+    partieFinie = police_grand.render(message, 1, 'black')
+
+    btn_menu = Bouton(menu, 400, 500)
+    btn_scores = Bouton(scores, 150, 500)
+
+    if btn_menu.affichage(ecran):
+        nouvelle_partie()
+        statut_partie = 0
+
+    if btn_scores.affichage(ecran):
+        statut_partie = 3
+
+    ecran.blit(partieFinie, (275, 175))
+    ecran.blit(reponse, (275, 250))
+
+
+def ecran_scores():
+
+    global statut_partie
+
+    ecran.fill((255, 255, 255))
+
+    for i in range(len(scores)):
+        ecran.blit(scores[i], (325, (50+(i*40))))
+
+    menu = police_petit.render("Menu", 1, 'black')
+    btn_menu = Bouton(menu, 600, 500)
+
+    if btn_menu.affichage(ecran):
+        nouvelle_partie()
+        statut_partie = 0
+
+
+def nouvelle_partie():
+
+    global solution, statut_pendu, correct, erreurs
+
+    statut_pendu = 0
+    correct = []
+    erreurs = []
+
+
+#teste si la lettre est presente dans le mot
+def test_lettre(lettre, solution):
+
+    global correct, erreurs, statut_pendu
+
+    if lettre in correct or lettre in erreurs or lettre not in string.ascii_lowercase:
+        return
+
+    if lettre in solution.lower():
+        correct += [lettre]
+    else:
+        erreurs += [lettre]
+        statut_pendu += 1
+
+
+def victoire():
+
+    for lettre in solution:
+        if not (lettre.lower() in correct):
+            return False
+
+    return True
+    
+
+en_cours = True
+
+# boucle principale
+while en_cours:
+    clock.tick(60)
+    match statut_partie:
+        case 0:
+            ecran_menu()
+        case 1:
+            ecran_jeu()
+        case 2:
+            ecran_fin()
+        case 3:
+            ecran_scores()
+
+    pygame.display.update()
+    
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            en_cours = False
+
+        if event.type == pygame.KEYDOWN and statut_partie == 1:
+
+            lettre = pygame.key.name(event.key)
+            test_lettre(lettre, solution)
+
+            if statut_pendu == 10 or victoire():
+
+                ajout_score(statut_pendu, solution, nom.texte)
+                scores = recup_scores(police_petit)
+                statut_partie = 2
+
+
+pygame.quit()
